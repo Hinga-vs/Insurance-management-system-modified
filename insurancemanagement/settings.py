036c9e34844ba498ea93c53020b7f207a73dbd2a
Hinga-vs <conradhinga@gmail.com>
2026-07-13 12:54:18 -0400

    Update ALLOWED_HOSTS for Render

--- insurancemanagement/settings.py
+++ insurancemanagement/settings.py
@@ -28,9 +28,10 @@ SECRET_KEY = 'ls@!_(edqp*xy76kvbsst$07at(v^li*2&ew!^$8o(@wa6@a+$'
 DEBUG = True
 
 ALLOWED_HOSTS = [
+    "insurance-management-system-modified.onrender.com",
     "insucover.onrender.com",
     "localhost",
-    "127.0.0.1"
+    "127.0.0.1",
 ]
 
 
